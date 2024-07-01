05efd26ae150a6a4ff42a41e7530e399d604292e
Caio Bacelar <caiobacelarM@hotmail.com>
2024-07-01 14:42:40 -0300

    Criado o script do personagem

---
+++ Character.cs
@@ -0,0 +1,40 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Character : MonoBehaviour
+{
+    public int life;
+    public Transform skin;
+    public Transform cam;
+    public Text heartCountText;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+      if(life <= 0)
+        {
+            skin.GetComponent<Animator>().Play("Die", -1);
+        }
+
+        if (transform.CompareTag("Player"))
+        {
+            heartCountText.text =  life.ToString();
+        }
+    }
+
+    public void PlayerDamage(int value)
+    {
+        life = life - value;
+        skin.GetComponent<Animator>().Play("PlayerDamage", 1);
+        cam.GetComponent<Animator>().Play("CameraPlayerDamage", -1);
+        GetComponent<PlayerController>().audioSource.PlayOneShot(GetComponent<PlayerController>().damageSound);
+    }
+}
